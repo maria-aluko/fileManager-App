f72c949d6c2362be7870730656b1ae9319f8764a
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-06 11:02:30 +0200

    add SinglePage component

---
+++ src/components/SinglePage.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export const SinglePage = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-4xl">Single Page</h1>
+      <Footer />
+    </div>
+  );
+};
